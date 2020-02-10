7f1effedfe334ffd4c5929292b6a7c52173459ad
plunderwondr <plunderwondr@gmail.com>
2020-02-10 16:36:38 -0500

    Converted  website to Javascript

---
+++ js/loadPage.js
@@ -0,0 +1,9 @@
+    $(document).ready(function()
+    {
+        $("#includes").load('includes.html'),
+        $("#navbar").load('navbar.html'),
+        $("#about").load('about.html'),
+        $("#portfolio").load('portfolio.html'),
+        $("#footer").load('footer.html'),
+        $("#contact").load('contact.html')
+    })
